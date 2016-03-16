ca1ed97da83aa056c013e7beb2b56f04a18d23db
wa05 <waltercejas19@hotmail.com>
2016-03-16 01:40:07 -0300

    First Commit

--- app/app.js
+++ app/app.js
@@ -5,8 +5,26 @@ angular.module('myApp', [
   'ngRoute',
   'myApp.view1',
   'myApp.view2',
+  'myApp.view3',
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+]).config(function ($routeProvider) {
+    $routeProvider.
+    when('/', {
+        templateUrl: 'view1/view1.html',
+        controller: 'View1Ctrl',
+        controllerAs: 'main'
+      }).
+      when('/about', {
+        templateUrl: 'view2/view2.html',
+        controller: 'View2Ctrl',
+        controllerAs: 'about'
+      }).
+      when('/contact', {
+        templateUrl: 'view3/view3.html',
+        controller: 'View3Ctrl',
+        controllerAs: 'contact'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
